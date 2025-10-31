b5cecd3e9c951d68fbab704af376c6eab4a922fd
lt-winter <minhdong9304@gmail.com>
2025-10-31 13:37:41 +0700

    Dinh nghia cac route cho tai nguyen Doc gia

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const readerRoutes = require("./app/routes/docgia.route");
+
+app.use("/api/readers", readerRoutes);
+
 app.get("/", (req, res) => {
   res.send("Welcome to the Book Backend API!");
 });
