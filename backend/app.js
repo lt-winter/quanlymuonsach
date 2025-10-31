b2663c211cf527f67b46e79e4185e7864ee35f85
lt-winter <minhdong9304@gmail.com>
2025-10-31 13:43:55 +0700

    Cai dat xu ly loi

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const ApiError = require("./app/api-error");
 
 const app = express();
 
@@ -7,6 +8,17 @@ app.use(cors());
 app.use(express.json());
 
 const readerRoutes = require("./app/routes/docgia.route");
+
+// handle 404 response
+app.use((req, res, next) => {
+  return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+  return res.status(err.statusCode || 500).json({
+    message: err.message || "Internal Server Error",
+  });
+});
 
 app.use("/api/readers", readerRoutes);
 
